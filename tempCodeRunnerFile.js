0e63c040191cac9749def82457d1ec37e5d37b68
Ajaj Anwar Anik <ajaj.anik180@gmail.com>
2025-09-27 14:48:47 +0600

    addding doMath & bill

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+function add (price1,price2){
+//     total = price1 + price2;
+//     return total;
+// }
+// const bill = add (20,70);
+
+// function add2(price1, price2){
+// return price1 + price2;
+// }
+// const bill2 = add (25,75);
+
+// console.log(bill,bill2);
