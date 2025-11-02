7cea8a83ed856509d48d21ceb4c98076ed800700
Ajaj Anwar Anik <ajaj.anik180@gmail.com>
2025-11-02 15:56:13 +0600

    addded output odd

---
+++ oddaverage.js
@@ -0,0 +1,10 @@
+function oddAverage(numbers){
+for (number of numbers){
+    if(number % 2 ===1){
+        console.log(number)
+    }
+}
+}
+const numbers = [42, 13, 58, 65, 81];
+const avg = oddAverage(numbers);
+console.log('average of the odd numbers is: ',avg);
